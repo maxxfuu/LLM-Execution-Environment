483aeeb5f4645753a1a8a6a53659650710421976
Max Fu <mfu27@ucmerced.edu>
2024-05-11 00:26:49 -0700

    necessary changes

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ from datasets import load_dataset
 
 # If the dataset is gated/private, make sure you have run huggingface-cli login
 dataset = load_dataset("uc-merced-pyedtools/test")
+
+print(dataset)
